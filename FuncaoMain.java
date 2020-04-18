a5f81e9ecc41c4f12938bd916ab087a7a1e7ce14
Gabriel Vieira <48156370+gabrielvieiraf@users.noreply.github.com>
2020-04-18 13:34:39 -0300

    Create FuncaoMain.java

---
+++ FuncaoMain.java
@@ -0,0 +1,9 @@
+//Java é uma linguagem orientada a objetos;
+//E em uma linguagem orientada a objetos, tudo parte de uma classe.
+
+public class Main {
+// A função main é responsável por tornar a classe Java em um programa Java. 
+    public static void main(String[] args) {
+        System.out.println("Exemplo 1 !");
+    }   
+}
