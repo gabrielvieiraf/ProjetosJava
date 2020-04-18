b2dc92ab5bb581956fb6f3aea64a4a926afef94a
Gabriel Vieira <48156370+gabrielvieiraf@users.noreply.github.com>
2020-04-18 19:56:15 -0300

    Update
    Adicionado novos comentários.

--- FuncaoMain.java
+++ FuncaoMain.java
@@ -1,9 +1,14 @@
 //Java é uma linguagem orientada a objetos;
-//E em uma linguagem orientada a objetos, tudo parte de uma classe.
+//Em uma linguagem orientada a objetos, tudo parte de uma classe.
 
 public class FuncaoMain {
 // A função main é responsável por tornar a classe Java em um programa Java. 
     public static void main(String[] args) {
+   
+ /*Println será responsável por mostrar na tela
+   o que está entre os parênteses. Você poderá imprimir
+   uma mensagem (String) a colocando entre aspas, como no exemplo abaixo*/
+ 
         System.out.println("Exemplo 1 !");
     }   
 }
